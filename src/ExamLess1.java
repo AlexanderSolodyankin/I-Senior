285df94bb7436a7b5d75cc3b387caea6e97ba777
Alexander <89515014507@mail.ru>
2022-11-28 20:23:02 +0600

    sadfghk

--- src/ExamLess1.java
+++ src/ExamLess1.java
@@ -14,19 +14,14 @@ public class ExamLess1 {
     }
 
     public static String getMoney(int num){
-        int cup1 = 200;
-        int cup2 = 500;
-        int cup3 = 1000;
-        int cup4 = 2000;
-        int cup5 = 5000;
 
         if(num <= 60000) {
-            for (int i = 0; i < (num / cup1) ; i++) {
-                for (int j = 0; j < (num / cup2) ; j++) {
-                    for (int k = 0; k < (num / cup3) ; k++) {
-                        for (int l = 0; l < (num / cup4) ; l++) {
-                            for (int m = 0; m < (num / cup5) ; m++) {
-                                int sum = (cup5 * m) + (cup4 * l) + (cup3 * k) + (cup2 * j) + (cup1 * i);
+            for (int i = 0; i < (num / 200) ; i++) {
+                for (int j = 0; j < (num / 500) ; j++) {
+                    for (int k = 0; k < (num / 1000) ; k++) {
+                        for (int l = 0; l < (num / 2000) ; l++) {
+                            for (int m = 0; m < (num / 5000) ; m++) {
+                                int sum = (5000 * m) + (2000 * l) + (1000 * k) + (500 * j) + (200 * i);
                                 if (num == sum) {
                                     return "5000:" + m + " 2000: " + l + " 1000: " + k + " 500: " + j + " 200: " + i;
                                 }
